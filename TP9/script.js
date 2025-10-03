440b6bacf3514b5402a87459515865f3b69d0e74
BigUknes <rayan.ghellab@gmail.com>
2025-10-03 09:51:29 +0200

    Add files via upload

---
+++ TP9/script.js
@@ -0,0 +1,7 @@
+function somme()
+{
+    var a = document.getElementById("n1").value;
+    var b = document.getElementById("n2").value;
+    var c = number(a)+number(b);
+    document.getElementById("r1").value=c;
+}
