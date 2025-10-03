90c90aacd5b3732d4a4bd4980e1c57d58a706882
BigUknes <rayan.ghellab@gmail.com>
2025-10-03 14:45:40 +0200

    Add files via upload

---
+++ TP10/script.js
@@ -0,0 +1,89 @@
+function somme() {
+  var a = document.getElementById("t1").value;
+  var b = document.getElementById("t2").value;
+
+  var c = Number(a) + Number(b);
+
+  document.getElementById("t3").value = c;
+}
+
+
+
+function soustraction() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+  
+    var c = Number(a) - Number(b);
+  
+    document.getElementById("t3").value = c;
+}
+
+
+
+  function multiplication() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+  
+    var c = Number(a) * Number(b);
+  
+    document.getElementById("t3").value = c;
+  }
+
+
+
+
+  function division() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+  
+    var c = Number (a) / Number (b);
+  
+    if (Number(b) === 0) 
+
+      document.getElementById("t3").value = "Erreur : on ne divise pas par 0";
+
+    else 
+    
+      document.getElementById("t3").value = c;
+    
+  }
+
+
+
+
+  function paire() {
+
+    var a = document.getElementById("t1").value;
+    var d = undefined;
+
+    if (a %2==0)
+    var d = "Paire";
+
+    else
+    var d = "Impaire";
+  
+    document.getElementById("t4").value = d;
+  }
+
+
+
+function permute() {
+  var a = document.getElementById("t1").value;
+  var b = document.getElementById("t2").value;
+  document.getElementById("t2").value = a;
+  document.getElementById("t1").value = b;
+  }
+
+
+
+function  effacer() {
+
+  document.getElementById("n1").value = "";
+  document.getElementById("n2").value = "";
+  document.getElementById("t3").value = "";
+  document.getElementById("t4").value = "";
+
+  }
+
+
+
